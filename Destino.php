ee793ad9fe0ba6d6a9d1768f23dd8840c0788817
carpio08 <carina.amuz@gmail.com>
2016-08-30 20:33:53 -0300

    con la carpeta de la guia del curso de ingreso

--- Destino.php
+++ Destino.php
@@ -1,16 +1,20 @@
 <?php
 
 $cantidad = strlen($_POST["mensaje"]);
-echo $cantidad;
+
 //Si se da determinada condicion incluyo un archivo u otro
+//Si quiero cambiar de pagina los archivos tienen q ser .php no .html
+//Si pongo .html lo incluyo en el archivo q estoy pero no me redirijo
+//SI EL ARCHIVO TIENE CODIGO PHP TIENE QUE SER .PHP, SINO NO HACE FALTA Y PUEDE SER HTML
 if($cantidad > 5){
-	include "MayorACinco.html";
+	include "MayorACinco.php";
 }else {
-	include "MenorACinco.html";;
+	include "MenorACinco.php";;
 }
 
+echo "VARIABLE _REQUEST <br>";
 var_dump($_REQUEST);
-echo "<br>";
+echo  "<br> VARIABLE POST <br>";
 var_dump($_POST["mensaje"]);
 
 ?>
---
+++ MayorACinco.php
@@ -0,0 +1,14 @@
+<html>
+	<head></head>
+	<body>
+		<form>
+			<div>MAYOR</div>
+			
+		</form>
+		<?php
+			echo "<h1>"."LA CADENA ES MAYOR A 5   " . $_POST["mensaje"] . "<br>"."</h1>";
+
+			?>
+	</body>
+</html>
+
